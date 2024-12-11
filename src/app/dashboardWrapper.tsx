ce83949d568703e522004969283b18836ab52707
Kashyap Mokariya <mokariyakashyap@gmail.com>
2024-12-11 20:25:14 +0530

    Created sidebar structure and added logos

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,14 +1,12 @@
 import Navbar from '@/components/Navbar'
-// import Sidebar from '@/components/Sidebar'
+import Sidebar from '@/components/Sidebar'
 import React, { ReactNode } from 'react'
-
-type Props = {}
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
             {/* Sidebar Code here */}
-            {/* <Sidebar /> */}
+            <Sidebar />
             <main className='dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64'>
                 {/* Navbar code here */}
                 <Navbar />
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { LockIcon } from 'lucide-react'
+import Image from 'next/image'
 import React, { useState } from 'react'
 
 const Sidebar = () => {
@@ -12,11 +14,33 @@ const Sidebar = () => {
     return (
         <div className={sidebarClasses}>
             <div className=' flex h-[100%] w-full flex-col justify-start'>
+
+                {/* Need to put out logo here */}
                 <div className='z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black'>
                     <div className='text-xl font-bold text-gray-800 dark:text-white'>
                         PlanIt
                     </div>
                 </div>
+
+                {/* Teams Section */}
+                <div className='flex items-center gap-5 border-y-[1.5px] border-gray-200 px-6 py-4 dark:border-gray-700'>
+                    <Image src="/Picwish_General_HD_2.jpeg" alt='logo' width={40} height={40} className='' />
+
+                    <div>
+                        <h3 className='text-md font-bold tracking-wide dark:text-gray-200'>
+                            PlanIt TEAM
+                        </h3>
+                        <div className='mt-1 flex items-start gap-2'>
+                            <LockIcon className='mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400' />
+                            <p className='text-xs text-gray-500'>
+                                Private
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Navbar links here */}
+                
             </div>
         </div>
     )
