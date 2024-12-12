120047c2b13e96e9d705c1957a40759b71bd9e27
Kashyap Mokariya <mokariyakashyap@gmail.com>
2024-12-12 16:15:21 +0530

    Integrated redux toolkit with dashboard wrapper and made some ui changes in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,11 @@
 "use client"
 
-import { LockIcon } from 'lucide-react'
+import { Home, Icon, LockIcon, LucideIcon } from 'lucide-react'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 import React, { useState } from 'react'
+import { useAppDispatch, useAppSelector } from './redux'
+import Link from 'next/link'
 
 const Sidebar = () => {
 
@@ -24,7 +27,7 @@ const Sidebar = () => {
 
                 {/* Teams Section */}
                 <div className='flex items-center gap-5 border-y-[1.5px] border-gray-200 px-6 py-4 dark:border-gray-700'>
-                    <Image src="/Picwish_General_HD_2.jpeg" alt='logo' width={40} height={40} className='' />
+                    <Image src="/main.jpeg" alt='logo' width={40} height={40} priority={true}/>
 
                     <div>
                         <h3 className='text-md font-bold tracking-wide dark:text-gray-200'>
@@ -32,7 +35,7 @@ const Sidebar = () => {
                         </h3>
                         <div className='mt-1 flex items-start gap-2'>
                             <LockIcon className='mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400' />
-                            <p className='text-xs text-gray-500'>
+                            <p className='text-xs text-gray-500 dark:text-white'>
                                 Private
                             </p>
                         </div>
@@ -40,9 +43,53 @@ const Sidebar = () => {
                 </div>
 
                 {/* Navbar links here */}
-                
+                <nav className='z-10 w-full'>
+                    <SidebarLink
+                        icon={Home}
+                        label="Home"
+                        href="/"
+                    />
+                </nav>
             </div>
         </div>
+    )
+}
+
+interface SidebarLinkProps{
+    href: string
+    icon: LucideIcon
+    label: string
+    // isCollapsed: boolean
+}
+
+const SidebarLink = ({
+    href,
+    icon: Icon,
+    label,
+    // isCollapsed
+}: SidebarLinkProps) => {
+    const pathName = usePathname()
+    const isActive = pathName === href || (pathName === "/" && href === "/dashboard")
+    const screenWidth = window.innerWidth
+
+    const dispatch = useAppDispatch()
+
+    const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed)
+
+    return (
+        <Link href={href} className='w-full'>
+            <div className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""}`}>
+                {isActive && (
+                    <div className='absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200' />
+                )}
+
+                <Icon className='h-6 w-6 text-gray-800 dark:text-gray-100' />
+
+                <span className={`font-medium text-gray-800 dark:text-gray-100`}>
+                    {label}
+                </span>
+            </div>
+        </Link>
     )
 }
 
