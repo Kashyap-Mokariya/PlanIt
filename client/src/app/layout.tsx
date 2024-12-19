d2d5c71d1dcf75c2bf430503ba4eb0125125d86e
Kashyap Mokariya <mokariyakashyap@gmail.com>
2024-12-19 20:09:41 +0530

    Changed folder structure to monorepo

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import "@/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+import DashboardWrapper from "./dashboardWrapper";
+
+export const metadata: Metadata = {
+  title: "PlanIt",
+  description: "Project Management App",
+  icons: [{ rel: "icon", url: "/main.ico"}],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body>
+        <DashboardWrapper>
+          {children}
+        </DashboardWrapper>
+      </body>
+    </html>
+  );
+}
